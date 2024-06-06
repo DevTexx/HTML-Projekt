764ee8f05655c75fca9203e79707013b3e2f9a0a
DevTex <stiers609@gmail.com>
2024-06-06 22:23:22 +0200

    On branch without-js Your branch is up to date with 'origin/without-js'.
    Changes to be committed:
    	modified:   de_DE/html/index.html
    	deleted:    de_DE/html/js/header.js
    	deleted:    de_DE/html/sites/impressum/js/header.js
    	deleted:    de_DE/html/sites/impressum/js/imprint_support_btn.js
    	deleted:    de_DE/html/sites/team/js/header.js
    	deleted:    de_DE/html/sites/vehicles/js/header.js
    	deleted:    de_DE/html/sites/vehicles/js/sounds.js
    	deleted:    de_DE/html/sites/vehicles/js/vehicle.js
    	modified:   de_DE/html/sites/vehicles/styles/styles.css
    	deleted:    de_DE/html/sites/vehicles/styles/styles_fmkw.css
    	deleted:    "de_DE/html/sites/vehicles/styles/styles_f\303\274komkw.css"
    	deleted:    "de_DE/html/sites/vehicles/styles/styles_f\303\274kw.css"
    	deleted:    de_DE/html/sites/vehicles/styles/styles_gkw.css
    	deleted:    de_DE/html/sites/vehicles/styles/styles_mtw.css
    	deleted:    de_DE/html/sites/vehicles/styles/styles_mzkw.css

--- de_DE/html/sites/impressum/js/imprint_support_btn.js
+++
@@ -1,7 +0,0 @@
-let supportButton = document.getElementById("getHelpWriteMail");
-
-function writeMailFunc(){
-    var emailTo = "mailto:support@relaxogames.de";
-    window = window.open(emailTo, "Kontaktiere Uns!")
-    console.info("FUNKTIONIERT");
-}
